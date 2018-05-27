364c4e73bb7e3a67bcf37393f6b14ee7d9051ff8
eden <eden.yk@gmail.com>
2018-05-27 14:55:37 +0800

    new update

--- Article/views.py
+++ Article/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from Article.models import Articles
 from django.http import Http404
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def home(request):
-    post_list = Articles.objects.all()
+    posts = Articles.objects.all()
+    paginator = Paginator(posts,6)
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger :
+        post_list = paginator.page(1)
+    except EmptyPage :
+        post_list = paginator.paginator(paginator.num_pages)
     return render(request,'home.html',{'post_list':post_list})
 
 def detail(request, id):
@@ -18,6 +27,29 @@ def detail(request, id):
 def archives(request):
     try:
         post_list=Articles.objects.all()
-    except:
+    except Articles.DoesNotExist:
         raise Http404
     return render(request, 'archives.html',{'post_list': post_list,'error':False})
+
+def search_tag(request, tag):
+    if tag==None:
+        return render(request,'tag.html',{"error":True})
+    else :
+        try:
+            post_list = Articles.objects.filter(category__iexact = tag)
+        except Articles.DoesNotExist:
+            raise Http404
+        return render(request, 'tag.html', {'post_list':post_list})
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Articles.objects.filter(title__icontains = s)
+            if len(post_list) == 0:
+                return render(request,'archives.html',{'post_list':post_list,'error':True})
+            else :
+                return render(request,'archives.html',{'post_list':post_list,'error':False})
+    return redirect('/')
